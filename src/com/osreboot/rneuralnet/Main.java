05f808611a2ff9620ea4e3f7c0ead3febaff197f
2001259 <2001259@IHS60261440.ad.nsd.org>
2015-09-25 12:22:46 -0700

    Initial commit

---
+++ src/com/osreboot/rneuralnet/Main.java
@@ -0,0 +1,27 @@
+package com.osreboot.rneuralnet;
+
+import com.osreboot.ridhvl.display.HvlDisplayMode;
+import com.osreboot.ridhvl.display.collection.HvlDisplayModeResizable;
+import com.osreboot.ridhvl.template.HvlTemplateInteg2D;
+
+public class Main extends HvlTemplateInteg2D{
+
+	public static void main(String args[]){
+		new Main();
+	}
+	
+	public Main(){
+		super(120, 1280, 720, "RNeuralNet - Neural Network / AI Testing", new HvlDisplayModeResizable());
+	}
+
+	@Override
+	public void initialize(){
+		
+	}
+
+	@Override
+	public void update(float delta){
+		
+	}
+
+}
